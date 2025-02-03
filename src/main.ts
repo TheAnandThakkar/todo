7e6b1db777c2b6deff673a4648f3ead194c1a440
Anand Thakkar <anand.thakkar92@gmail.com>
2025-02-03 09:22:30 +0530

    validation pipeline added for class transformer

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +17,23 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+
+      exceptionFactory: (errors: ValidationError[]) => {
+        const firstError = errors[0];
+        const message = firstError.constraints
+          ? Object.values(firstError.constraints)[0]
+          : 'Validation error';
+
+        return new BadRequestException(message);
+      },
+    }),
+  );
 
   await app.listen(configService.get('PORT')!);
 }
